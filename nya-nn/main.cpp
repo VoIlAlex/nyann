a4eefada82b9633e0cd3e706f7c2c4adc3948e84
Ilya <ilya.vouk@gmail.com>
2019-09-16 20:31:56 +0300

    Fix data set in main

--- nya-nn/main.cpp
+++ nya-nn/main.cpp
@@ -14,10 +14,10 @@ int main()
 	// A dataset for the problem
 	// dataset_x -> dataset_y
 	nyann::TrainDataSet<float> dataset = {
-		{{1.5, 1.5}, {0}},
-		{{1.5, 0.5}, {1}},
-		{{0.5, 1.5}, {}},
-		{{0.5, 0.5}, {}}
+		{{1.5, 1.5}, {0.}},
+		{{1.5, 0.5}, {1.}},
+		{{0.5, 1.5}, {1.}},
+		{{0.5, 0.5}, {0.}}
 	};
 
 	// nn layers
