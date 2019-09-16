12cc0134a02ad698359d9fd44bfe757aa13c698b
Ilya <ilya.vouk@gmail.com>
2019-09-16 21:10:47 +0300

    Update layer declaration

--- nya-nn/nyann/Layer.h
+++ nya-nn/nyann/Layer.h
@@ -8,8 +8,13 @@ namespace nyann
 	class Layer
 	{
 	public:
-		Layer() = delete;
-		virtual DataSet<_DT_OUT> operator() (const DataSet& <_DT_IN>);
+		virtual DataSet<_DT_OUT> operator() (const DataSet<_DT_IN>&) = 0;
+		virtual DataRow<_DT_OUT> operator() (const DataRow<_DT_IN>&) = 0;
+		// 1-dimensional for now
+		virtual std::vector<double> back_propagation(
+			const std::vector<double>& errors,
+			const std::vector<double>& derivitives, // d(yj) / d(Sj) - caused by activation function
+			double lr = 0.01) = 0;
 	};
 
 } // namespace nyann
