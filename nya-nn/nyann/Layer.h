f269de7d54c414d4f70f0a9175e9b0315cb40049
VoIlAlex <ilya.vouk@gmail.com>
2019-09-16 17:31:09 +0300

    Add an interface of Layer

--- nya-nn/nyann/Layer.h
+++ nya-nn/nyann/Layer.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#include "dataset.h"
+
+namespace nyann
+{
+	template<typename _DT_IN, typename _DT_OUT = _DT_IN>
+	class Layer
+	{
+	public:
+		Layer() = delete;
+		virtual DataSet<_DT_OUT> operator() (const DataSet& <_DT_IN>);
+	};
+
+} // namespace nyann
+
 
 // include subclasses of Layer
 #include "Layer/FCLayer.h"
