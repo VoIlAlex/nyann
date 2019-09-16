76a2511f8ba5fcffc127970cb69a72131ee9afd2
Ilya <ilya.vouk@gmail.com>
2019-09-16 22:53:34 +0300

    Add Net.Fitting test

--- nyann-tests/test.cpp
+++ nyann-tests/test.cpp
@@ -80,4 +80,24 @@ TEST(Net, LayersSetting)
 	net.add_layer(layer_2);
 }
 
+
+TEST(Net, Fitting)
+{
+	nyann::Layer<double>* layer_1 = new nyann::FCLayer<double>(nyann::Size(2, 2));
+	nyann::Layer<double>* layer_2 = new nyann::FCLayer<double>(nyann::Size(2, 1));
+	nyann::Net net;
+	net.add_layer(layer_1);
+	net.add_layer(layer_2);
+
+	nyann::TrainDataSet<double> dataset = {
+		{{1.5, 1.5}, {0.}},
+		{{1.5, 0.5}, {1.}},
+		{{0.5, 1.5}, {1.}},
+		{{0.5, 0.5}, {0.}}
+	};
+
+	net.fit(dataset, 10);
+}
+
+
 }
