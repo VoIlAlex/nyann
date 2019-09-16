3dda2cad614ab8396f498f6409249620d2e8bc2d
VoIlAlex <ilya.vouk@gmail.com>
2019-09-16 16:43:27 +0300

    Add an interface for Size

--- nya-nn/nyann/utils/Size.h
+++ nya-nn/nyann/utils/Size.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <vector>
+
+namespace nyann {
+
+	class Size : public std::vector<int>
+	{
+	public:
+		using std::vector<int>::vector;
+	};
+
+} // namespace nyann
--- nyann-tests/test.cpp
+++ nyann-tests/test.cpp
@@ -3,11 +3,16 @@
 
 
 
-TEST(DataSet, DataSet)
+TEST(DataSet, DataSet_construction)
 {
 	nyann::DataRow<float> row = { 1.0, 2.0, 3.0 };
 	nyann::DataSet<float> dataset = {
 		{1.0, 2.0},
 		{3.0, 4.0}
 	};
+}
+
+TEST(Size, Size_construction)
+{
+	nyann::Size size{ 2, 5, 10 };
 }
