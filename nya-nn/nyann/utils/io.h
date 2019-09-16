a7f16928fad55d0224dea019d0711ca4e679bf15
Ilya <ilya.vouk@gmail.com>
2019-09-16 20:33:55 +0300

    Add IO for DataRow

--- nya-nn/nyann/utils/io.h
+++ nya-nn/nyann/utils/io.h
@@ -27,5 +27,15 @@ std::ostream& operator<<(std::ostream& stream, const nyann::DataSet<_DT>& datase
 	return stream;
 }
 
+template<typename _DT>
+std::ostream& operator<<(std::ostream& stream, const nyann::DataRow< _DT>& datarow)
+{
+	stream << "[";
+	for (int i = 0; i < datarow.size(); i++)
+	{
+		if (i != 0) stream << ", ";
+		stream << datarow[i];
+	}
+	stream << "]";
 	return stream;
 }
