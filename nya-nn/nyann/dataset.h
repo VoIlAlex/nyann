8a9b2104a92e2b36e9d8bd70b00f8126890a4813
Ilya <ilya.vouk@gmail.com>
2019-09-16 20:47:03 +0300

    Add operators for DataRow

--- nya-nn/nyann/dataset.h
+++ nya-nn/nyann/dataset.h
@@ -20,6 +20,47 @@ namespace nyann {
 	public:
 		using std::vector<DataRow<_DT>>::vector;
 	};
+
+	////////////////////////////
+	// Operators overloadings //
+	////////////////////////////
+	template<typename _DT>
+	DataRow<_DT> operator+(const DataRow<_DT>& left, const DataRow<_DT>& right)
+	{
+		if (left.size() != right.size())
+			throw DifferentSizeError();
+
+		DataRow<_DT> result(left.size());
+		for (int i = left.size(); i < right.size(); i++)
+			result[i] = left[i] + right[i];
+		return result;
+	}
+
+	template<typename _DT>
+	DataRow<_DT> operator-(const DataRow<_DT>& left, const DataRow<_DT>& right)
+	{
+		if (left.size() != right.size())
+			throw DifferentSizeError();
+
+		DataRow<_DT> result(left.size());
+		for (int i = left.size(); i < right.size(); i++)
+			result[i] = left[i] - right[i];
+		return result;
+	}
+
+	template<typename _DT>
+	DataRow<_DT> operator-(const DataRow<_DT>& obj)
+	{
+		DataRow<_DT> result(obj);
+
+		for (int i = 0; i < obj.size(); i++)
+			result[i] = -result[i];
+
+		return result;
+	}
+
+
+
 } // namespace nyann
 
 
