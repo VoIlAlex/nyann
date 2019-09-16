2da5fcad8b4d47aec5f1d7f4e314e193e6075a66
VoIlAlex <ilya.vouk@gmail.com>
2019-09-16 16:17:51 +0300

    Add nyann.h header to keep all the headers

--- nya-nn/nyann.h
+++ nya-nn/nyann.h
@@ -1,1 +1,11 @@
 #pragma once
+
+
+// include all the headers of 
+// nyann library
+
+#include "nyann/dataset.h"
+#include "nyann/Layer.h"
+#include "nyann/Loss.h"
+#include "nyann/Optimizer.h"
+#include "nyann/Net.h"
