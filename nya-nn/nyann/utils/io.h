8305a56705ab7588a48c31d88718d5e1ce7e221c
VoIlAlex <ilya.vouk@gmail.com>
2019-09-16 17:30:18 +0300

    Add IO for DataFrame

--- nya-nn/nyann/utils/io.h
+++ nya-nn/nyann/utils/io.h
@@ -1,2 +1,28 @@
 #pragma once
+#include <iostream>
 
+#include "..//dataset.h"
+
+template<typename _DT>
+std::ostream& operator<<(std::ostream& stream, const nyann::DataSet<_DT>& dataset)
+{
+	std::cout << "[[";
+	for (int i = 0; i < dataset.size(); i++)
+	{
+		if (i != 0)
+			std::cout << "[";
+
+		for (int j = 0; j < dataset[i].size(); j++)
+		{
+			if (j != 0)
+				std::cout << ", ";
+			std::cout << dataset[i][j];
+		}
+		std::cout << "]";
+		if (i != dataset.size() - 1)
+			std::cout << ", " << std::endl;
+	}
+	std::cout << "]";
+
+	return stream;
+}
