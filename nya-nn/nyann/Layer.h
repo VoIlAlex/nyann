28873abc6d22f2cf838227fa034b4f50ccba07d9
Ilya <ilya.vouk@gmail.com>
2019-09-17 23:09:46 +0300

    Update Layer interface

--- nya-nn/nyann/Layer.h
+++ nya-nn/nyann/Layer.h
@@ -9,11 +9,15 @@ namespace nyann
 	{
 	public:
 		virtual DataSet<_DT_OUT> operator() (const DataSet<_DT_IN>&) = 0;
-		virtual DataRow<_DT_OUT> operator() (const DataRow<_DT_IN>&) = 0;
+		virtual DataRow<_DT_OUT> operator() (const DataRow<_DT_IN>&, bool save = true) = 0;
 		// 1-dimensional for now
 		virtual std::vector<double> back_propagation(
 			const std::vector<double>& errors,
 			const std::vector<double>& derivitives, // d(yj) / d(Sj) - caused by activation function
+			double lr = 0.01) = 0;
+		virtual DataSet<double> back_propagation(
+			const DataSet<double>& errors,
+			const DataSet<double>& derivitives, // d(yj) / d(Sj) - caused by activation function
 			double lr = 0.01) = 0;
 	};
 
