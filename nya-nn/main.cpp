90c963d1040c8f6c05788137c1b6e43f0fed5782
Ilya <ilya.vouk@gmail.com>
2019-09-25 11:43:23 +0300

    Update main.cpp

--- nya-nn/main.cpp
+++ nya-nn/main.cpp
@@ -11,80 +11,12 @@
 
 int main()
 {
-	//// A dataset for the problem
-	//// dataset_x -> dataset_y
-	//nyann::TrainDataSet<double> dataset = {
-	//	{{1.5, 1.5}, {0.}},
-	//	{{1.5, 0.5}, {1.}},
-	//	{{0.5, 1.5}, {1.}},
-	//	{{0.5, 0.5}, {0.}}
-	//};
-
-	//// nn layers
-	//nyann::Layer<double>* layer_1 = new nyann::FCLayer<double>(nyann::Size{ 2, 5 });
-	//layer_1->add_activation_function(new nyann::ReLU<double>());
-	//nyann::Layer<double>* layer_2 = new nyann::FCLayer<double>(nyann::Size{ 5, 5 });
-	//layer_2->add_activation_function(new nyann::ReLU<double>());
-	//nyann::Layer<double>* layer_3 = new nyann::FCLayer<double>(nyann::Size{ 5, 1 });
-	//layer_3->add_activation_function(new nyann::ReLU<double>());
-
-	//// creatinga  neural network
-	//nyann::Net net;
-	//net.add_layer(layer_1);
-	//net.add_layer(layer_2);
-	//net.add_layer(layer_3);
-
-	////// setting an loss function
-	////nyann::Loss* loss = new myann::MSELoss();
-	////net.set_loss(loss);
-
-
-	////// setting an optimizer
-	////nyann::Optimizer* opt = new nyann::SGDOptimizer(0.01);
-	////net.set_optimizer(opt);
-
-	//// train the network
-	//net.fit(
-	//	dataset, // dataset
-	//	20, // epoch number
-	//	2, // batch size
-	//	0.03
-	//);
-
-	//// Predict a value
-	//nyann::DataSet<double> predicted_values = net.predict({
-	//	{1.5, 1.5},
-	//	{1.5, 0.5},
-	//	{0.5, 1.5},
-	//	{0.5, 0.5}
-	//	});
-
-	//// visualize the results
-	//std::cout << predicted_values;
-
-
 	nyann::TrainDataSet<double> dataset = nyann::generate_linear_dataset<double>(1000, 5, 0.2);
 	dataset.shuffle();
 	nyann::TrainDataSet<double> dataset_train = { dataset.begin(), dataset.begin() + 800 };
 	nyann::TrainDataSet<double> dataset_test = { dataset.begin() + 800, dataset.end() };
 
 	nyann::Net net;
-
-	// TODO: add control under 
-	// size of input/output of 
-	// nearly located layers
-	/*nyann::Layer<double>* layer_1 = new nyann::FCLayer<double>(nyann::Size{ 5, 1000 });
-	layer_1->add_activation_function(new nyann::ReLU<double>(0.05));
-	nyann::Layer<double>* layer_2 = new nyann::FCLayer<double>(nyann::Size{ 1000, 20 });
-	layer_2->add_activation_function(new nyann::ReLU<double>(0.05));
-	nyann::Layer<double>* layer_3 = new nyann::FCLayer<double>(nyann::Size{ 20, 1 });
-	layer_3->add_activation_function(new nyann::ReLU<double>(0.05));*/
-
-	/*net.add_layer(layer_1);
-	net.add_layer(layer_2);
-	net.add_layer(layer_3);*/
-
-	// FIXIT: I CANNOT MAKE IT WORK!!!
 
 	nyann::Layer<double>* layer_1 = new nyann::FCLayer<double>(nyann::Size{ 5, 5 });
 	nyann::Layer<double>* layer_2 = new nyann::FCLayer<double>(nyann::Size{ 5, 1 });
