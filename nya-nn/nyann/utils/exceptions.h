f4d3e8e78e415532862ff9cb45b8631dddeabd88
Ilya <ilya.vouk@gmail.com>
2019-10-01 16:15:18 +0300

    Add index error exceptions hierarchy

--- nya-nn/nyann/utils/exceptions.h
+++ nya-nn/nyann/utils/exceptions.h
@@ -5,7 +5,25 @@
 namespace nyann {
 
 	class DifferentSizeError : public ::std::exception {};
+
 	class ConversionError : public ::std::exception {};
-	class IndexOverflowError : public ::std::exception {};
+
+	class IndexError : public ::std::out_of_range
+	{
+	public:
+		using ::std::out_of_range::out_of_range;
+	};
+
+	class IndexSizeError : public IndexError
+	{
+	public:
+		using IndexError::IndexError;
+	};
+
+	class IndexOverflowError : public IndexSizeError
+	{
+	public:
+		using IndexSizeError::IndexSizeError;
+	};
 
 } // namespace nyann
