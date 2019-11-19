37299d298dde2bcf307f78200da9c574b236dc8e
Ilya <ilya.vouk@gmail.com>
2019-11-19 19:56:18 +0300

    BipolarActivation for batch processing

--- nyann/nyann/ActivationFunction/BipolarActivation.h
+++ nyann/nyann/ActivationFunction/BipolarActivation.h
@@ -21,18 +21,33 @@ namespace nyann {
 		{
 
 			DataSet<_DT> results = DataSet<_DT>::ones_like(sums);
-#ifdef DRAFT_DATASET
+#ifdef DRAFT_DATASET_2_0_0_ALPHA_TEST
 			for (auto it = sums.begin(-1); it != sums.end(-1); it++)
 			{
 				// TODO: update for the new style DataSet
 			}
+#else
+			for (int i = 0; i < sums.get_size()[0]; i++)
+				for (int j = 0; j < sums.get_size()[1]; i++)
+					results[i][j] = operator()(sums[i][j]);
 #endif
 			return results;
 
 		};
 		DataSet<_DT> derivative(const DataSet<_DT>& Y) override
 		{
-			return{}; // TODO: implement
+			DataSet<_DT> results = DataSet<_DT>::ones_like(Y);
+#ifdef DRAFT_DATASET_2_0_0_ALPHA_TEST
+			for (auto it = sums.begin(-1); it != sums.end(-1); it++)
+			{
+				// TODO: update for the new style DataSet
+			}
+#else
+			for (int i = 0; i < Y.get_size()[0]; i++)
+				for (int j = 0; j < Y.get_size()[1]; i++)
+					results[i][j] = derivative(Y[i][j]);
+#endif
+			return results;
 		}
 	};
 
