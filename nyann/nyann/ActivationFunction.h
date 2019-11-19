a6a018201ace00acd1c8f1e70f97deb318149f54
Ilya <ilya.vouk@gmail.com>
2019-11-19 19:55:30 +0300

    Make abstract class out of Activation Function

--- nyann/nyann/ActivationFunction.h
+++ nyann/nyann/ActivationFunction.h
@@ -10,8 +10,36 @@ namespace nyann {
 	public:
 		virtual _DT operator()(const _DT& sum) = 0;
 		virtual _DT derivative(const _DT& y) = 0;
-		virtual DataSet<_DT> operator()(const DataSet<_DT>& sums) = 0;
-		virtual DataSet<_DT> derivative(const DataSet<_DT>& Y) = 0;
+		virtual DataSet<_DT> operator()(const DataSet<_DT>& sums)
+		{
+			DataSet<_DT> results = DataSet<_DT>::ones_like(sums);
+#ifdef DRAFT_DATASET_2_0_0_ALPHA_TEST
+			for (auto it = sums.begin(-1); it != sums.end(-1); it++)
+			{
+				// TODO: update for the new style DataSet
+			}
+#else
+			for (int i = 0; i < sums.get_size()[0]; i++)
+				for (int j = 0; j < sums.get_size()[1]; i++)
+					results[i][j] = operator()(sums[i][j]);
+#endif
+			return results;
+		}
+		virtual DataSet<_DT> derivative(const DataSet<_DT>& Y)
+		{
+			DataSet<_DT> results = DataSet<_DT>::ones_like(Y);
+#ifdef DRAFT_DATASET_2_0_0_ALPHA_TEST
+			for (auto it = sums.begin(-1); it != sums.end(-1); it++)
+			{
+				// TODO: update for the new style DataSet
+			}
+#else
+			for (int i = 0; i < Y.get_size()[0]; i++)
+				for (int j = 0; j < Y.get_size()[1]; i++)
+					results[i][j] = derivative(Y[i][j]);
+#endif
+			return results;
+		}
 		// TODO: save activation function 
 		// identifier
 		virtual void save(const std::string& filename) const
