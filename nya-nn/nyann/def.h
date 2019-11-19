f6b3ee2b2950d5e011922124bb9fc8331dcf0de0
VoIlAlex <ilya.vouk@gmail.com>
2019-11-19 07:44:43 +0300

    Add definition file-controller

---
+++ nya-nn/nyann/def.h
@@ -0,0 +1,13 @@
+/*
+    This is a controll file.
+    All the definitions here 
+    will affect behavior of 
+    the framework.
+*/
+
+#pragma once
+
+#define GROUP_ACTIVATION
+
+// Draft features togglers
+// #define DRAFT_DATASET
