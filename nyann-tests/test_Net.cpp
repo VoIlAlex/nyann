d291fde1fa9c6dca90f199c233e82b789e167ae9
VoIlAlex <ilya.vouk@gmail.com>
2019-12-14 01:06:54 +0300

    Test training output for plotting

--- nyann-tests/test_Net.cpp
+++ nyann-tests/test_Net.cpp
@@ -35,4 +35,23 @@ namespace {
 
 		net.fit(dataset, 10);
 	}
+
+	TEST(Net, OutputForPlot)
+	{
+		nyann::Layer<double>* layer_1 = new nyann::FCLayer<double>(nyann::Size<>{ 2, 2 });
+		nyann::Layer<double>* layer_2 = new nyann::FCLayer<double>(nyann::Size<>{ 2, 1 });
+		nyann::Net net;
+		net.add_layer(layer_1);
+		net.add_layer(layer_2);
+
+		nyann::TrainDataSet<double> dataset = {
+			{{1.5, 1.5}, {0.}},
+			{{1.5, 0.5}, {1.}},
+			{{0.5, 1.5}, {1.}},
+			{{0.5, 0.5}, {0.}}
+		};
+
+		std::vector<double> errors = net.fit(dataset, 10);
+		ASSERT_EQ(errors.size(), 10);
+	}
 } // namespace
