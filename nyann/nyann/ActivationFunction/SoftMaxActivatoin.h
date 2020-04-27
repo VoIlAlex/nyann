6c197b943b0b78c8820cb982a37b64ec6d30a3e3
VoIlAlex <ilya.vouk@gmail.com>
2020-04-27 20:27:30 +0300

    Add SoftMax for new-style DataSet

--- nyann/nyann/ActivationFunction/SoftMaxActivatoin.h
+++ nyann/nyann/ActivationFunction/SoftMaxActivatoin.h
@@ -52,6 +52,35 @@ public:
 	}
 };
 #elif defined(DRAFT_DATASET_2_0_0_PRERELEASE)
+	template <typename _DT>
+	class SoftMax : public ActivationFunction<_DT>
+	{
+	public:
+		_DT operator()(const _DT &sum) override
+		{
+			throw NotImplementedError("Single processing isn't supported by SoftMax");
+		}
+		_DT derivative(const _DT &y) override
+		{
+			return y * (1 - y);
+		}
+
+		DataSet<_DT> operator()(const DataSet<_DT> &sums) override
+		{
+			DataSet<_DT> results = DataSet<_DT>::ones_like(sums);
+			_DT sum_of_exponents = _DT();
+			_DT exponent;
+			for (int i = 0; i < sums.data().size(); i++)
+			{
+				exponent = exp(sums.data()[i]);
+				results.access_data()[i] = exponent;
+				sum_of_exponents += exponent;
+			}
+			for (int i = 0; i < results.data().size(); i++)
+				results.access_data()[i] /= sum_of_exponents;
+			return results;
+		}
+	};
 #endif
 
 } // namespace nyann
