569fa81df1ac9a769b6690ae5f275ebe46f90641
VoIlAlex <ilya.vouk@gmail.com>
2020-05-12 17:01:05 +0300

    Add test_exceptions.cpp file

---
+++ nyann-tests/test_exceptions.cpp
@@ -0,0 +1,113 @@
+#include "pch.h"
+#include "../nyann/nyann.h"
+
+namespace {
+
+	TEST(FrameworkExceptions, DifferentSizeError)
+	{
+		try
+		{
+			throw nyann::DifferentSizeError("Error");
+		}
+		catch (nyann::DifferentSizeError &e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, ConversionError)
+	{
+		try
+		{
+			throw nyann::ConversionError("Error");
+		}
+		catch (nyann::ConversionError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, SizeError)
+	{
+		try
+		{
+			throw nyann::SizeError("Error");
+		}
+		catch (nyann::SizeError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, IndexError)
+	{
+		try
+		{
+			throw nyann::IndexError("Error");
+		}
+		catch (nyann::IndexError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, IndexSizeError)
+	{
+		try
+		{
+			throw nyann::IndexSizeError("Error");
+		}
+		catch (nyann::IndexSizeError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, IndexOverflowError)
+	{
+		try
+		{
+			throw nyann::IndexOverflowError("Error");
+		}
+		catch (nyann::IndexOverflowError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, NotImplementedError)
+	{
+		try
+		{
+			throw nyann::NotImplementedError("Error");
+		}
+		catch (nyann::NotImplementedError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, ConstParentError)
+	{
+		try
+		{
+			throw nyann::ConstParentError("Error");
+		}
+		catch (nyann::ConstParentError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+
+	TEST(FrameworkExceptions, DeprecationError)
+	{
+		try
+		{
+			throw nyann::DeprecationError("Error");
+		}
+		catch (nyann::DeprecationError & e)
+		{
+			EXPECT_EQ(std::string(e.what()), "Error");
+		}
+	}
+} // namespace
