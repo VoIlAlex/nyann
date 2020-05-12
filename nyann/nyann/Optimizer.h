b56aef6cba8df7f250b525ef450d7d8d45f299a9
VoIlAlex <ilya.vouk@gmail.com>
2020-05-12 16:52:27 +0300

    Implement Optimizer

--- nyann/nyann/Optimizer.h
+++ nyann/nyann/Optimizer.h
@@ -4,10 +4,51 @@
 // For all the framework configurations
 #include "_config.h"
 
+#include "DataSet.h"
+
+#include <sstream>
+
 
 namespace nyann {
 
-	class Optimizer {};
+	template<typename _DT = double>
+	class Optimizer : public Serializable<std::string>
+	{
+	protected:
+		_DT m_lr;
+
+	public:
+		Optimizer(_DT lr)
+			: m_lr(lr)
+		{}
+
+		virtual void operator()(
+			DataSet<_DT>& weights,
+			DataSet<_DT>& biases,
+			const DataSet<_DT>& derivatives,
+			const DataSet<_DT>& errors,
+			const DataSet<_DT>& input,
+			const Size<>& size_in,
+			const Size<>& size_out,
+			int batch_size) const = 0;
+
+		std::string serialize() const override
+		{
+			std::ostringstream result;
+			result << m_lr;
+			return result.str();
+		}
+
+		void deserialize(std::istringstream& data) override
+		{
+			std::string raw_lr;
+			std::getline(data, raw_lr);
+			m_lr = to_type<_DT>(raw_lr);
+		}
+
+	};
+
+	
 
 } // namespace nyann
 
