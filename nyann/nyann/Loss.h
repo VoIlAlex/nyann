cf445737d0d812a75ee8dbc1345b286855003de8
VoIlAlex <ilya.vouk@gmail.com>
2020-05-12 16:51:49 +0300

    Add Loss class

--- nyann/nyann/Loss.h
+++ nyann/nyann/Loss.h
@@ -4,10 +4,27 @@
 // For all the framework configurations
 #include "_config.h"
 
+#include "dataset.h"
+#include "utils/Serializer.h"
 
 namespace nyann {
 
-	class Loss {};
+	template<typename _DT>
+	class Loss : public Serializable<std::string>
+	{
+	public:
+		virtual DataSet<_DT> operator()(const DataSet<_DT>& output, const DataSet<_DT>& target) = 0;
+		virtual DataSet<_DT> derivative(const DataSet<_DT>& loss) = 0;
+		std::string serialize() const override
+		{
+			return "";
+		}
+		void deserialize(std::istringstream& stream) override
+		{
+			std::string empty_line;
+			std::getline(stream, empty_line);
+		}
+	};
 
 } // namespace nyann
 
