ffbe4e9c4c3cbaa543225746f768fd149d4cd760
VoIlAlex <ilya.vouk@gmail.com>
2020-05-12 16:31:28 +0300

    Fix ReLUActivation class name

--- nyann/nyann/ActivationFunction/ReLUActivation.h
+++ nyann/nyann/ActivationFunction/ReLUActivation.h
@@ -10,18 +10,20 @@
 namespace nyann {
 
 	template<typename _DT>
-	class ReLU : public ActivationFunction<_DT>
+	class ReLUActivation : public ActivationFunction<_DT>
 	{
 		double m_k;
 
 	public:
-		ReLU(double k = 0.2)
+		ReLUActivation(double k = 0.0)
 			: m_k(k)
 		{}
+
 		_DT operator()(const _DT& sum)
 		{
 			return (sum > 0 ? sum : m_k * sum);
 		}
+
 		_DT derivative(const _DT& y) override
 		{
 			return (y > 0 ? 1 : m_k);
